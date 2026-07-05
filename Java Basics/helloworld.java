03963dc749e998058b9a35c83abfc9e2e7f2b19a
Gajanan Gite <gajanangite30102004@gmail.com>
2026-07-05 19:48:47 +0530

    Update helloworld.java

--- Java Basics/helloworld.java
+++ Java Basics/helloworld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-         // we can also use like  
-        // System.out.print("Hello, World!");
+        //  println() 
+        //  print() when you want output on the same line (e.g., printing arrays).
+        //  printf() when you need nicely formatted output, especially for numbers like percentages or decimal values
       
     }
 }
