da8924c082997f9f28d9453664293f0963d6c7b3
Gajanan Gite <gajanangite30102004@gmail.com>
2026-07-05 19:45:17 +0530

    Create helloworld.java

---
+++ Java Basics/helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+         // we can also use like  
+        // System.out.print("Hello, World!");
+      
+    }
+}
