410244d694e611d1f32e67797f96fd1f644967d8
Gajanan Gite <gajanangite30102004@gmail.com>
2026-07-05 19:49:59 +0530

    Create variables.java

---
+++ Java Basics/variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Integer
+        int age = 20;
+
+        // Floating-point number
+        float height = 5.8f;
+
+        // Double
+        double pi = 3.14159;
+
+        // Character
+        char grade = 'A';
+
+        // Boolean
+        boolean isStudent = true;
+
+        // String
+        String name = "Gajanan";
+
+        // Printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
