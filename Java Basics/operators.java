59b443cf939e5c912e252c1dea87aa1e5c106997
Gajanan Gite <gajanangite30102004@gmail.com>
2026-07-05 20:00:03 +0530

    Create operators.java

---
+++ Java Basics/operators.java
@@ -0,0 +1,28 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational Operators
+        System.out.println("\na > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+
+        // Logical Operators
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("\nx && y : " + (x && y));
+        System.out.println("x || y : " + (x || y));
+        System.out.println("!x : " + (!x));
+    }
+}
