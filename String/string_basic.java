32c5589df4dce8ea65bb62e1ed1d59600a8d2e5c
Gajanan Gite <gajanangite30102004@gmail.com>
2026-07-10 20:33:47 +0530

    Create string_basic.java

---
+++ String/string_basic.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.util.Scanner;
+
+public class string_basic {
+    
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        // input in string
+        String name;
+        name=sc.nextLine();
+        name=sc.next();
+
+        // length of string 
+        name.length();//gives length of string name
+                        // it is fuction
+
+        // Array.length;   
+        // gives aaray length 
+        // its a property of an array
+
+        // concatination of string= adding two strings
+
+        name.charAt(0);
+        // gives char at index 0th locarion
+    }
+}
