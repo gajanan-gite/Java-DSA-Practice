f2aa119c809a54b73bf35537988b1baf4262f75c
Gajanan Gite <gajanangite30102004@gmail.com>
2026-07-10 20:37:26 +0530

    Create string_palindrome.java

---
+++ String/string_palindrome.java
@@ -0,0 +1,34 @@
+package string;
+import java.util.Scanner;
+
+public class string_palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+
+}
